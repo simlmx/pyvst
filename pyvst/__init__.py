7ea7d7d5c46b29869db8a28aedf9ce06f1bda6e5
Simon Lemieux <lemieux.simon@gmail.com>
2018-09-15 14:41:43 -0400

    wrap in a python package

---
+++ pyvst/__init__.py
@@ -0,0 +1,1 @@
+from .pyvst import VstPlugin
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='pyvst',
+    version='0.1',
+    description='VST2.4 python wrapping using ctypes',
+    author='Simon Lemieux',
+    author_email='lemieux.simon (at) gmail (dot) (you know what)',
+    url='',
+    packages=['pyvst'],
+)
